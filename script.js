0d8e550e1483a31cc6abe0c4f94524b5a73481ee
Shadow-Nic <nicbasler@gmail.com>
2023-08-09 10:41:00 +0200

    Ich Opfer vergesse es sonst

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let mybutton = document.getElementById("backTop");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 300) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 900) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
